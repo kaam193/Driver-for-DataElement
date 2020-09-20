21145b7a33009bf4c0beadd249b9d220abca3dfe
kaam193 <70541128+kaam193@users.noreply.github.com>
2020-09-19 20:31:05 -0400

    Add files via upload

---
+++ MovieDriver.java
@@ -0,0 +1,45 @@
+package driver;
+
+import java.util.Scanner;
+
+public class MovieDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner keyboard = new Scanner(System.in);
+		Movie theMovie = new Movie();
+		String title;
+		String rating;
+		int tickets;
+		char anwser;
+
+
+		do {
+			System.out.println("Enter the name of a movie");
+			title = keyboard.nextLine();
+			theMovie.setTitle(title);
+
+			System.out.println("Enter the rating of the movie");
+			rating = keyboard.nextLine();
+			theMovie.setRating(rating);
+
+			System.out.println("Enter the number of tickets sold for this movie");
+			tickets = keyboard.nextInt();
+			theMovie.setSoldTickets(tickets);
+			System.out.println(theMovie.toString());
+
+			System.out.println("Do you want to enter another movie? (y or n)");
+			keyboard.nextLine();
+			anwser =  keyboard.next().charAt(0);
+
+		}while(anwser == 'y'); 
+
+
+
+
+	}
+
+
+
+}
